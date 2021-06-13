b8b9f44e4917a430ce2c0f1ef22bc12ab507f187
Trainex <lexuantu1002@gmail.com>
2021-06-13 21:08:27 +0700

    Create

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ThongTinYTe.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
