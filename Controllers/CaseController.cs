81eefd4be15a736f0863c1169b20f48d4ed281af
thanhdanh2401 <lephamthanhdanh@gmail.com>
2021-07-13 13:51:15 +0700

    CanhBaoDiChuyen and Warning for quarantine

--- Controllers/CaseController.cs
+++ Controllers/CaseController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Net.Http;
 using System.Net.Http.Headers;
@@ -21,7 +22,7 @@ namespace ThongTinYTe.Controllers
         }
 
         // public IActionResult Index() {
-        //     return View(_service.GetRecords());
+        //     return View(_service_detail.GetRecords());
         // }
 
         public async Task<IActionResult> KhaiBaoToanDan()
@@ -52,7 +53,67 @@ namespace ThongTinYTe.Controllers
 
             return View();
         }
+        [HttpPost]
+        public IActionResult KhaiBaoToanDan(KhaiBaoToanDan people)
+        {
+            IEnumerable<CaseRecord> records = _service_detail.GetRecords();
+            foreach (CaseRecord record in records)
+            {
+                if (record.Location == people.tinh_thanh)
+                {
+                    return View("CanhBaoKhuVuc");
+                }
+            }
+            return RedirectToAction("Index", "Home");
+        }
+
+
+
+        public async Task<IActionResult> KhaiBaoDiChuyen()
+        {
+            // Lấy danh sách thành phố ở Việt Nam từ service
+            IEnumerable<ProvinceRecord> records = _service.GetRecords();
+            List<string> city_list = new List<string>();
+            foreach (ProvinceRecord record in records)
+            {
+                city_list.Add(record.Province);
+            }
+            ViewBag.cities = city_list;
+            // Lấy danh sách quốc gia từ api
+            HttpClient client = new HttpClient();
+            var contentType = new MediaTypeWithQualityHeaderValue("application/json");
+            client.DefaultRequestHeaders.Accept.Add(contentType);
+            string countries_api = "https://restcountries.eu/rest/v2/all";
+            HttpResponseMessage response_countries_api = await client.GetAsync(countries_api);
+            string raw_cities = await response_countries_api.Content.ReadAsStringAsync();
+            List<CountriesAPI> countries_object = JsonConvert.DeserializeObject<List<CountriesAPI>>(raw_cities);
+            List<string> countries_list = new List<string>();
+            foreach (var country in countries_object)
+            {
+                countries_list.Add(country.name);
+            }
+            ViewBag.countries = countries_list; 
+
+            return View();
+        }
+        [HttpPost]
+        public IActionResult KhaiBaoDiChuyen(KhaiBaoDiChuyen traveler)
+        {
+            IEnumerable<CaseRecord> records = _service_detail.GetRecords();
+            foreach (CaseRecord record in records)
+            {
+                if (record.Location == traveler.noi_di || record.Location == traveler.noi_den)
+                {
+                    return View("CanhBaoCachLy");
+                }
+            }
+            return RedirectToAction("Index", "Home");
+        }
+
     }
+
+
+
     public class CountriesAPI
     {
         public string name { get; set; }
