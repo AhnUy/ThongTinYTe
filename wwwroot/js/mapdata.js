4d7278e38668ff9913e592146191a7328725b95f
codeforcent <tuankietdxd123@gmail.com>
2021-07-14 14:25:45 +0700

    Vietnam map

---
+++ wwwroot/js/mapdata.js
@@ -0,0 +1,273 @@
+var simplemaps_countrymap_mapdata={
+  main_settings: {
+    //General settings
+		width: "300", //or 'responsive'
+    background_color: "#FFFFFF",
+    background_transparent: "yes",
+    border_color: "#ffffff",
+    pop_ups: "detect",
+    
+		//State defaults
+		state_description: "State description",
+    state_color: "#88A4BC",
+    state_hover_color: "#3B729F",
+    state_url: "",
+    border_size: 1.5,
+    all_states_inactive: "no",
+    all_states_zoomable: "yes",
+    
+		//Location defaults
+		location_description: "Location description",
+    location_url: "",
+    location_color: "#FF0067",
+    location_opacity: 0.8,
+    location_hover_opacity: 1,
+    location_size: 25,
+    location_type: "square",
+    location_image_source: "frog.png",
+    location_border_color: "#FFFFFF",
+    location_border: 2,
+    location_hover_border: 2.5,
+    all_locations_inactive: "no",
+    all_locations_hidden: "no",
+    
+		//Label defaults
+		label_color: "#d5ddec",
+    label_hover_color: "#d5ddec",
+    label_size: 22,
+    label_font: "Arial",
+    hide_labels: "no",
+    hide_eastern_labels: "no",
+   
+		//Zoom settings
+		zoom: "yes",
+    manual_zoom: "yes",
+    back_image: "no",
+    initial_back: "no",
+    initial_zoom: "-1",
+    initial_zoom_solo: "no",
+    region_opacity: 1,
+    region_hover_opacity: 0.6,
+    zoom_out_incrementally: "yes",
+    zoom_percentage: 0.99,
+    zoom_time: 0.5,
+    
+		//Popup settings
+		popup_color: "white",
+    popup_opacity: 0.9,
+    popup_shadow: 1,
+    popup_corners: 5,
+    popup_font: "12px/1.5 Verdana, Arial, Helvetica, sans-serif",
+    popup_nocss: "no",
+    
+		//Advanced settings
+		div: "map",
+    auto_load: "yes",
+    url_new_tab: "no",
+    images_directory: "default",
+    fade_time: 0.1,
+    link_text: "View Website"
+  },
+  state_specific: {
+    VNM429: {
+      name: "Quang Ninh",
+      description: "default",
+      color: "red",
+      hover_color: "default",
+      url: "default"
+    },
+    VNM444: {
+      name: "Tay Ninh"
+    },
+    VNM450: {
+      name: "Đien Bien"
+    },
+    VNM451: {
+      name: "Đong Bac"
+    },
+    VNM452: {
+      name: "Thai Nguyen"
+    },
+    VNM453: {
+      name: "Lai Chau"
+    },
+    VNM454: {
+      name: "Lang Son"
+    },
+    VNM455: {
+      name: "Son La"
+    },
+    VNM456: {
+      name: "Thanh Hoa"
+    },
+    VNM457: {
+      name: "Tuyen Quang"
+    },
+    VNM458: {
+      name: "Yen Bai"
+    },
+    VNM459: {
+      name: "Hoa Binh"
+    },
+    VNM460: {
+      name: "Hai Duong"
+    },
+    VNM4600: {
+      name: "Hai Phong"
+    },
+    VNM461: {
+      name: "Dong Bang Song Hong"
+    },
+    VNM462: {
+      name: "Ha Noi"
+    },
+    VNM463: {
+      name: "Bac Ninh"
+    },
+    VNM464: {
+      name: "Vinh Phuc"
+    },
+    VNM466: {
+      name: "Ninh Binh"
+    },
+    VNM467: {
+      name: "Ha Nam"
+    },
+    VNM468: {
+      name: "Nam Dinh"
+    },
+    VNM469: {
+      name: "Phu Tho"
+    },
+    VNM470: {
+      name: "Bac Giang"
+    },
+    VNM471: {
+      name: "Thai Binh"
+    },
+    VNM474: {
+      name: "Ha Tinh"
+    },
+    VNM475: {
+      name: "Nghe An"
+    },
+    VNM476: {
+      name: "Quang Binh"
+    },
+    VNM477: {
+      name: "Dak Lak"
+    },
+    VNM478: {
+      name: "Gia Lai"
+    },
+    VNM479: {
+      name: "Khanh Hoa"
+    },
+    VNM480: {
+      name: "Lam Dong"
+    },
+    VNM481: {
+      name: "Ninh Thuan"
+    },
+    VNM482: {
+      name: "Phu Yen"
+    },
+    VNM483: {
+      name: "Binh Dương"
+    },
+    VNM4834: {
+      name: "Tien Giang"
+    },
+    VNM4835: {
+      name: "Dak Nong"
+    },
+    VNM484: {
+      name: "Binh Phuoc"
+    },
+    VNM485: {
+      name: "Binh Dịnh"
+    },
+    VNM486: {
+      name: "Kon Tum"
+    },
+    VNM487: {
+      name: "Quang Nam"
+    },
+    VNM488: {
+      name: "Quang Ngai"
+    },
+    VNM489: {
+      name: "Quang Tri"
+    },
+    VNM490: {
+      name: "Thua Thien - Hue"
+    },
+    VNM491: {
+      name: "Da Nang"
+    },
+    VNM495: {
+      name: "Ba Ria - Vung Tau"
+    },
+    VNM496: {
+      name: "Binh Thuan"
+    },
+    VNM497: {
+      name: "Dong Nam Bo"
+    },
+    VNM498: {
+      name: "An Giang"
+    },
+    VNM499: {
+      name: "Can Tho"
+    },
+    VNM500: {
+      name: "Dong Thap"
+    },
+    VNM501: {
+      name: "Ho Chi Minh city"
+    },
+    VNM502: {
+      name: "Kien Giang"
+    },
+    VNM503: {
+      name: "Long An"
+    },
+    VNM504: {
+      name: "Ben Tre"
+    },
+    VNM505: {
+      name: "Hau Giang"
+    },
+    VNM506: {
+      name: "Bac Lieu"
+    },
+    VNM507: {
+      name: "Ca Mau"
+    },
+    VNM508: {
+      name: "Soc Trang"
+    },
+    VNM509: {
+      name: "Tra Vinh"
+    },
+    VNM510: {
+      name: "Vinh Long"
+    },
+    VNM511: {
+      name: "Cao Bang"
+    },
+    VNM512: {
+      name: "Ha Giang"
+    },
+    VNM5483: {
+      name: "Lao Cai"
+    }
+  },
+  locations: {
+    "0": {
+      lat: "21.028167",
+      lng: "105.854152",
+      name: "Hanoi"
+    }
+  }
+};
